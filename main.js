169916320c1d6442dd487418c281eac6e86e3672
Neal Valdez <nealvaldez10@Hotmail.com>
2025-07-16 21:35:44 -0400

    last min changes

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-const btn  = document.querySelector('.hamburger-btn');
-    const menu = document.querySelector('.hamburger-menu');
-
-    btn.addEventListener('click', () => {
-        menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-    });
-
-    // Click outside to close
-    document.addEventListener('click', e => {
-        if (!btn.contains(e.target) && !menu.contains(e.target)) {
-        menu.style.display = 'none';
-        }
-    });
-
-
 const answers = {
     q1: "B",
     q2: "B",
